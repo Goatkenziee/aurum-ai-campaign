0118312e744179ff271deeaa19c48152e63ae2e9
Goatkenziee <mackquisition@gmail.com>
2026-05-31 02:36:53 -0400

    Add home page implementation

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className='flex flex-col items-center justify-center min-h-screen'>
+      <h1 className='text-4xl font-bold'>Welcome to Aurum AI Campaign</h1>
+      <p className='mt-4'>This is your content campaign management platform.</p>
+      <button className='px-6 py-3 mt-6 bg-blue-600 text-white rounded hover:bg-blue-700'>Get Started</button>
+    </main>
+  );
+}
